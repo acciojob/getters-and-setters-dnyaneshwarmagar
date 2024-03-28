6b85e44c4ea41ce0abb5d2d27464928794851a44
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-29 03:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ class Student extends Person {
 		super(name,age)
 	}
 	study(){
-		console.log(`${super.name} is studying`)
+		console.log(`${this.name} is studying`)
 	}
 }
 
@@ -29,9 +29,21 @@ class Teacher extends Person {
 		super(name,age)
 	}
 	teach(){
-		console.log(`${super.name} is teaching`)
+		console.log(`${this.name} is teaching`)
 	}
 }
+
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
